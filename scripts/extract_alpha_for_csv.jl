6c232508eacb8d29e9e03e9d7951b6f625a82376
Casano Kirlew <150434408+Cas64288@users.noreply.github.com>
2025-08-28 12:56:02 +0100

    Update extract_alpha_for_csv.jl

--- scripts/extract_alpha_for_csv.jl
+++ scripts/extract_alpha_for_csv.jl
@@ -1,24 +1,20 @@
+# If you have CSV data from using the 2D_backflow_edit_un.jl
+# You can extract alpha values from it 
+# Probably won't be needed 
 using CSV
 using DataFrames
 using Printf
-# Path to your combined CSV:
 input_csv = "nodal_surface_smooth.csv"
 
-# Read entire file once
 df = CSV.read(input_csv, DataFrame)
 
-# Get the distinct alpha values, sorted
 alphas = sort(unique(df.alpha))
 
 for α in alphas
-    # Filter rows where alpha == this value
     sub = df[df.alpha .== α, :]
-
-    # Construct output filename, e.g. nodal_surface_alpha_0.0.csv
+    # Note: my naming conventions, nodal_surface_alpha_0.0.csv ext
     fname = @sprintf("nodal_surface_alpha_%.1f.csv", α)
-
-    # Write out the subset
     println("Writing $(nrow(sub)) rows to $fname …")
     CSV.write(fname, sub)
 end
-println("Done! Split into $(length(alphas)) files.")
+println("Done")
